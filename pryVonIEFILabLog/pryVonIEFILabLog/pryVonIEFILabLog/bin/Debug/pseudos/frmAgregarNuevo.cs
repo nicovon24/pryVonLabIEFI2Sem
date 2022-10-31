f0ef9386d557f5d84028eec3cf2050eabf9d2847
nicovon24 <nicovon24@gmail.com>
2022-10-31 12:49:21 -0300

    commit

--- pryVonIEFILabLog/pryVonIEFILabLog/pryVonIEFILabLog/bin/Debug/pseudos/frmAgregarNuevo.cs
+++ pryVonIEFILabLog/pryVonIEFILabLog/pryVonIEFILabLog/bin/Debug/pseudos/frmAgregarNuevo.cs
@@ -1,17 +1,20 @@
 inicio procedimiento agregarNuevo
     //---variables---
     //numerica
-    int dni = parseInt(txtDNI.text), edad = parseInt(nudEdad.text),
-    idBarrio = parseInt(nudBarrio.text), idSucursal = parseInt(nudSucursal.text), idProfesor = parseInt(nudProfesor.text), idActividad = parseInt(txtActuvudad.text)
+    int dni = parseInt(txtDNI.text), deuda = parseInt(nudDeuda.text)
     
     //texto
     string nombre = txtNombre.text, char sexo = txtSexo.text, telefono = txtTel.text, email = txtEmail.text
+    string strBarrio = cbBarrio.text, strSucursal = cbSucursal.text, strProfesor = cbProfesor.text, strActividad = cbActividad.text
+
+    //char
+    char edad = cbEdad.text
 
     //banderas
     bool flagEstaCodEnClientes = false
 
-    //variables de id a string, luego almacenamos x ej el nombre del profesor aca
-    string barrio, sucursal, profesor, actividad
+    //variables de string a id, luego almacenamos x ej el nombre del profesor aca
+    int idBarrio, idSucursal, idProfesor, idActividad
 
     //---me fijo si esta el codigo en clientes---
     abrir y leer add.clientes
@@ -21,10 +24,83 @@ inicio procedimiento agregarNuevo
     si add.clientes.eof
         flagEstaCodEnClientes = true
     fin si
+    cerrar add.clientes
 
     //---programa---
     si(flagEstaCodEnClientes==false)
+        //--abro y uso los demás archivos para buscar el id de barrio, sucursal, profesor y actividad
+        //barrios
+        abrir y leer add.barrios
+        mientras not add.barrios.eof y add.barrios.detalle<>barrio
+            leer add.barrios
+        fin mientras
+
+        mientras not add.barrios.eof y add.barrios.detalle==barrio
+            idBarrio = add.barrios.cod_barrio
+            leer add.barrios
+        fin mientras
+        cerrar add.barrios
+
+
+
+        //sucursales
+        abrir y leer add.sucursales
+        mientras not add.sucursales.eof y add.sucursales.detalle<>sucursal
+            leer add.sucursales
+        fin mientras
+
+        mientras not add.sucursales.eof y add.sucursales.detalle==sucursal
+            idSucursal = add.sucursales.cod_sucursal
+            leer add.sucursales
+        fin mientras
+        cerrar add.sucursales
+
+
+
+        //profesores
+        abrir y leer add.profesores
+        mientras not add.profesores.eof y add.profesores.nombre<>strProfesor
+            leer add.profesores
+        fin mientras
+
+        mientras not add.profesores.eof y add.profesores.nombre==strProfesor
+            idProfesor = add.profesores.cod_barrio
+            leer add.profesores
+        fin mientras
+        cerrar add.profesores
+
+
+
+        //actividades
+        abrir y leer add.actividades
+        mientras not add.actividades.eof y add.actividades.detalle<>strActividad
+            leer add.actividades
+        fin mientras
+
+        mientras not add.actividades.eof y add.actividades.detalle==strActividad
+            idActividad = add.actividades.cod_barrio
+            leer add.actividades
+        fin mientras
+        cerrar add.actividades
         
+
+        //---connecting to the data base clientes and adding data into it---
+        string urlDB = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=BD_Clientes.accdb";
+
+        sqlTables = "Insert into Clientes (DNI, [Nombre y apellido], Sexo, Deuda, ID_barrio, ID_sucursal, ID_profesor, ID_actividad, Teléfono, Email) " +
+        
+        sqlValues = dni + ", '" + nombre + "', '" + sexo + "', " + deuda + ", " + idBarrio + ", " + idSucursal + ", " + idProfesor + ", "
+        + idActividad + ", '" + telefono + "', '" + email + "'"
+
+        OleDbConnection dbConnection = new OleDbConnection(urlDB);
+        dbConnection.Open();
+        OleDbCommand comando = new OleDbCommand();
+        comando.Connection = dbConnection;
+        comando.CommandType = CommandType.Text;
+        comando.CommandText = "INSERT INTO Clientes (" + sqlTables + ") VALUES(" + sqlValues + ")";
+        comando.ExecuteNonQuery();
+        MessageBox.Show("Dato cargado");
+        dbConnection.Close();
     fin si
 
     sino
